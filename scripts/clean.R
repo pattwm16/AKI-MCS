35d30915afbd0f906095bb7a1a3a6f76f0b24632
Will Patterson <wmp26@case.edu>
2023-11-21 09:43:36 -0500

    edit gitignore for vscode settings

--- scripts/clean.R
+++ scripts/clean.R
@@ -1,6 +1,4 @@
-# cleaning script
-
-# load in libraries for cleaning
+## load in libraries for cleaning
 library(janitor)
 library(tidyverse)
 library(readxl)
@@ -9,11 +7,4 @@ library(readxl)
 source("scripts/helpers.R")
 
 # get path to raw excel file
-path <- "../data/"
-
-path %>%
-  excel_sheets() %>%
-  set_names() %>% 
-  map(read_then_csv, path = path)
-
-dir(pattern = "^data.*\\.csv$")
+source("data/dataLoad.R")
