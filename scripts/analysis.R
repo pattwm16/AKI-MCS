07cb71d2a916c5f3b25b8ddcdafbee5602b046ef
Will Patterson <wmp26@case.edu>
2023-12-11 16:31:52 -0500

    Edit plot 1 of RRT requirements.

--- scripts/analysis.R
+++ scripts/analysis.R
@@ -11,28 +11,6 @@ library(tidyverse)
 data <- read_csv("data/cleaned_analysis_data.csv")
 matched_data <- read_csv("data/cleaned_matched_data.csv")
 
-# primary hypothesis ---
-# patients in CS w/ tMCS have lower survival to hospital discharge rate
-# when additionally needing RRT
-
-# identify patients in CS w/ tMCS
-# treatment is rrt
-# outcome is hosp_surv_yn
-matched_data %>%
-  filter(cs_etiology != "No shock") %>%
-  group_by(rrt_yn) %>%
-  tabyl(rrt_yn, hosp_surv_yn)
-
-# Fit the logistic regression model
-matched_data %>%
-  filter(cs_etiology != "No shock") %>%
-  glm(hosp_surv_yn ~ rrt_yn, family = binomial, weights = weights, data = .) %>%
-  tbl_regression(., exponentiate = TRUE) %>%
-
-  # save as .docx
-  as_gt() %>%
-  gt::gtsave(filename = "tbls/regs/primary_hypothesis.docx")
-
 # secondary hypothesis 1 ---
 # survival to hospital discharge in patients treated with tMCS or CS suffering
 # from AKI correlates inversely with the severity of AKI-stadium
--- scripts/primary_hypothesis1.R
+++ scripts/primary_hypothesis1.R
@@ -42,15 +42,24 @@ proportions <- matched_data %>%
     group_by(cs_etiology, rrt_yn, hosp_surv_yn) %>%
     summarise(n = n(), .groups = "drop") %>%
     mutate(prop = n / sum(n)) %>%
-    mutate(hosp_surv_yn = recode(as.character(hosp_surv_yn), "TRUE" = "Survived to discharge", "FALSE" = "Died prior to discharge"))
+    mutate(hosp_surv_yn = recode(as.character(hosp_surv_yn), "TRUE" = "Survived to discharge", "FALSE" = "Died prior to discharge")) %>%
+    mutate(cs_etiology = forcats::fct_relevel(cs_etiology, "No shock")) %>%
+    mutate(cs_etiology = forcats::fct_relevel(cs_etiology, "Other", , after = length(levels(cs_etiology)))) %>%
+    mutate(hosp_surv_yn = forcats::fct_rev(hosp_surv_yn))
 
 # Create the bar chart
-p1 <- ggplot(proportions, aes(x = cs_etiology, y = prop, fill = rrt_yn)) +
+(p1 <- ggplot(proportions, aes(x = cs_etiology, y = prop, fill = rrt_yn)) +
     geom_bar(stat = "identity", position = "fill", alpha = 0.6) +
+    geom_text(aes(label = n), position = position_fill(vjust = 0.5), size = 3) +
     scale_fill_manual(values = c("TRUE" = "#DAA520", "FALSE" = "lightgrey"), labels = c("TRUE" = "Required", "FALSE" = "Not required")) +
     facet_grid(hosp_surv_yn ~ .) +
     scale_y_continuous(labels = scales::percent) +
-    labs(x = "CS Etiology", y = "", fill = "Need for RRT") +
-    hrbrthemes::theme_ipsum()
+    labs(x = "Etiology of cardiogenic shock",
+         y = "",
+         fill = "Need for RRT",
+         caption = "RRT was defined as one or more days of either hemodialysis or continuous hemofiltration while undergoing tMCS",
+         subtitle = "Patients with tMCS"
+        ) +
+    hrbrthemes::theme_ipsum(grid = "Y"))
 
 ggsave("figs/need_rrt_by_hosp_surv.png", plot = p1, bg = 'white')
