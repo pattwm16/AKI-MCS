a3844e06d005685d1fe755185f33d3f221cf9051
Will Patterson <wmp26@case.edu>
2023-12-11 14:21:32 -0500

    Organized analyses and created regs folder under tbls

--- scripts/analysis.R
+++ scripts/analysis.R
@@ -2,12 +2,16 @@
 
 # libraries
 library(MatchIt)
+library(broom)
+library(janitor)
+library(gtsummary)
 library(tidyverse)
 
 # data load
 data <- read_csv("data/cleaned_analysis_data.csv")
 matched_data <- read_csv("data/cleaned_matched_data.csv")
 
+# primary hypothesis ---
 # patients in CS w/ tMCS have lower survival to hospital discharge rate
 # when additionally needing RRT
 
@@ -19,28 +23,34 @@ matched_data %>%
   group_by(rrt_yn) %>%
   tabyl(rrt_yn, hosp_surv_yn)
 
+# Fit the logistic regression model
+matched_data %>%
+  filter(cs_etiology != "No shock") %>%
+  glm(hosp_surv_yn ~ rrt_yn, family = binomial, weights = weights, data = .) %>%
+  tbl_regression(., exponentiate = TRUE) %>%
 
+  # save as .docx
+  as_gt() %>%
+  gt::gtsave(filename = "tbls/regs/primary_hypothesis.docx")
 
+# secondary hypothesis 1 ---
+# survival to hospital discharge in patients treated with tMCS or CS suffering
+# from AKI correlates inversely with the severity of AKI-stadium
 
-fit <- lm(hosp_surv_yn ~ rrt_yn * (age + sex + bmi + lactate + vis_score),
-          data = matched_data, weights = weights)
+# ANALYSIS TBD...
 
-plot(fit)
+# secondary hypothesis 2 ---
+# survival to hospital discharge in ECMELLA patients with AKI is superior to
+# va-ECLS patients with AKI (dichotomous)
 
-marginaleffects::avg_comparisons(fit,
-                variables = "rrt_yn",
-                vcov = ~subclass,
-                newdata = subset(matched_data, rrt_yn == 0),
-                wts = "weights")
+# ECMELLA pts vs. va-ECLS pts + aki_yn
+matched_data %>%
+  group_by(group) %>%
+  tabyl(aki_yn, hosp_surv_yn)
 
-# sec 1
-
-# sec 2
-fit1 <- lm(death ~ aki_yn * (age + sex + bmi + lactate + vis_score),
-          data = matched_data, weights = weights)
-
-marginaleffects::avg_comparisons(fit1,
-                                 variables = "aki_yn",
-                                 vcov = ~subclass,
-                                 newdata = subset(matched_data, aki_yn == 1),
-                                 wts = "weights")
+# fit logistic regression model
+matched_data %>%
+  glm(hosp_surv_yn ~ aki_yn + group, family = binomial, weights = weights, data = .) %>%
+  tbl_regression(., exponentiate = TRUE) %>%
+  as_gt() %>%
+  gt::gtsave(filename = "tbls/regs/secondary_hypothesis_2.docx")
