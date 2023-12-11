bd879f920066f23787781b9528dcf6b773aeb032
Will Patterson <wmp26@case.edu>
2023-12-11 18:43:50 -0500

    Linting

--- tbls/table1.R
+++ tbls/table1.R
@@ -3,45 +3,49 @@ library(tidyverse)
 library(flextable)
 library(table1)
 
-data <- read_csv("data/cleaned_analysis_data.csv")
+# read data
+data <- read_csv("data/cleaned_analysis_data.csv") %>%
+  mutate(cs_etiology = forcats::fct_relevel(cs_etiology, "No shock")) %>%
+  mutate(cs_etiology = forcats::fct_relevel(cs_etiology, "Other", , after = length(levels(cs_etiology))))
 
-# parse label
-label(data$age) <- "Age"
-label(data$sex) <- "Sex"
-label(data$bmi) <- "BMI"
-label(data$diabetes) <- "Type II Diabetes" # TODO: I assume?
-label(data$ckd_yn) <- "Chronic kidney disease"
-label(data$ckd_stage) <- "Chronic kidney disease stage"
-label(data$copd_yn) <- "Chronic obstructive pulmonary disease"
-label(data$copd_stage) <- "Chronic obstructive pulmonary disease stage"
-label(data$lactate) <- "Lactate"
-label(data$med_ph) <- "Median pH"
-label(data$med_cr) <- "Median creatinine"
-label(data$vis_score) <- "Vasoactive-inotropic score (VIS)"
-label(data$pre_rrt_yn) <- "RRT prior to tMCS"
-label(data$mi_yn) <- "Previous cardiac arrest"
-label(data$ecpr) <- "eCPR"
-label(data$rx_nephrotox) <- "Nephrotoxic drugs"
-label(data$cs_etiology) <- "Cause of cardiogenic shock"
+# parse labels and units
+label(data$age)          <- "Age"
+label(data$sex)          <- "Sex"
+label(data$bmi)          <- "BMI"
+label(data$diabetes)     <- "Diabetes (Type II)"     # TODO: I assume?
+label(data$ckd_yn)       <- "Chronic kidney disease"
+label(data$ckd_stage)    <- "Chronic kidney disease (stage)ᵃ"
+label(data$copd_yn)      <- "Chronic obstructive pulmonary disease"
+label(data$copd_stage)   <- "Chronic obstructive pulmonary disease (stage)"
+label(data$lactate)      <- "Lactate"
+label(data$med_ph)       <- "Median pH"
+label(data$med_cr)       <- "Median creatinine"
+label(data$vis_score)    <- "Vasoactive-inotropic score (VIS)ᵇ"
+label(data$pre_rrt_yn)   <- "RRT required (prior to tMCS)"
+label(data$mi_yn)        <- "Previous cardiac arrest"
+label(data$ecpr)         <- "eCPR"
+label(data$rx_nephrotox) <- "Nephrotoxic drugsᶜ "
+label(data$cs_etiology)  <- "Etiology of cardiogenic shock"
 
 units(data$age)     <- "years"
 units(data$lactate) <- "mg/dL" # TODO: is this the right unit?
 units(data$med_cr)  <- "mg/dL" # TODO: is this the right unit?
 
+footnote <- paste0("ᵃ Stage 3a and 3b were coded as 3 and 4, respectively. The remaining stages were increased by one value.<br>", "ᵇ VIS, a weighted sum of vasopressor and inotropic medications, quantifies the amount of pharmacological cardiovascular support in patients on tMCS.<br>", "ᶜ  Vancomycin (IV), gentamycin, or tobramycin")
 
 # create table 1
 tab1 <- data %>%
   table1(
-  ~ age + sex + bmi + diabetes + ckd_yn + ckd_stage + copd_yn + copd_stage +
-    lactate + med_ph + vis_score + pre_rrt_yn + med_cr + cs_etiology + mi_yn + 
-    ecpr + rx_nephrotox | group, 
-  data = .
+    ~ age + sex + bmi + diabetes + ckd_yn + ckd_stage + copd_yn + copd_stage +
+      lactate + med_ph + vis_score + pre_rrt_yn + med_cr + cs_etiology + mi_yn +
+      ecpr + rx_nephrotox | group,
+    data = ., footnote = footnote
   )
 
 # save tables as .docx and image
-t1flex(tab1) %>% 
-  save_as_docx(path="tbls/table1.docx")
+t1flex(tab1) %>%
+  save_as_docx(path = "tbls/table1.docx")
 
-t1flex(tab1) %>% 
+t1flex(tab1) %>%
   bg(bg = "white", part = "all") %>%
-  save_as_image(path="tbls/table1.png")
+  save_as_image(path = "tbls/table1.png")
--- tbls/table2.R
+++ tbls/table2.R
@@ -4,7 +4,7 @@ library(flextable)
 library(table1)
 
 #load("../data/cleaned_analysis_data.Rda")
-constructed <- read_csv("data/cleaned_matched_data.csv") %>%
+data <- read_csv("data/cleaned_matched_data.csv") %>%
   mutate(
     aki_s1 = as.logical(aki_s1),
     aki_s2 = as.logical(aki_s2),
@@ -14,39 +14,39 @@ constructed <- read_csv("data/cleaned_matched_data.csv") %>%
     aki_s1 = replace_na(aki_s1, FALSE),
     aki_s2 = replace_na(aki_s2, FALSE),
     aki_s3 = replace_na(aki_s3, FALSE)
-  ) 
+  )
 
 # parse label
-label(constructed$aki_s1) <- "AKI - Stadium 1"
-label(constructed$aki_s2) <- "AKI - Stadium 2"
-label(constructed$aki_s3) <- "AKI - Stadium 3"
-label(constructed$pre_rrt_yn) <- "RRT prior to tMCS"
-label(constructed$rrt_duration) <- "Duration of RRT"
-label(constructed$vent_duration) <- "Duration of ventilation"
-#label(constructed$icu_los) <- "Length of ICU stay"
-label(constructed$hosp_los) <- "Length of hospital stay"
-label(constructed$hosp_surv_yn) <- "Survival to hospital discharge"
+label(data$aki_s1)          <- "AKI - Stadium 1"
+label(data$aki_s2)          <- "AKI - Stadium 2"
+label(data$aki_s3)          <- "AKI - Stadium 3"
+label(data$pre_rrt_yn)      <- "RRT prior to tMCS"
+label(data$rrt_duration)    <- "Duration of RRT"
+label(data$vent_duration)   <- "Duration of ventilation"
+#label(data$icu_los)        <- "Length of ICU stay"
+label(data$hosp_los)        <- "Length of hospital stay"
+label(data$hosp_surv_yn)    <- "Survival to hospital discharge"
 
-units(constructed$rrt_duration)  <- "days"
-units(constructed$vent_duration) <- "days"
-#units(constructed$icu_los) <- "days"
-units(constructed$hosp_los) <- "days"
+units(data$rrt_duration)    <- "days"
+units(data$vent_duration)   <- "days"
+#units(data$icu_los)        <- "days"
+units(data$hosp_los)        <- "days"
 
 
 # create table 2
-tab2 <- constructed %>%
+tab2 <- data %>%
   table1(
-  ~ aki_s1 + aki_s2 + aki_s3 + pre_rrt_yn + pre_rrt_yn + rrt_duration + 
-    vent_duration +
-    # icu_los +
-    hosp_los + hosp_surv_yn | group, 
-  data = .
+    ~ aki_s1 + aki_s2 + aki_s3 + pre_rrt_yn + pre_rrt_yn + rrt_duration +
+      vent_duration +
+      # icu_los +
+      hosp_los + hosp_surv_yn | group,
+    data = .
   )
 
 # save tables as .docx and image
-t1flex(tab2) %>% 
-  save_as_docx(path="tbls/table2.docx")
+t1flex(tab2) %>%
+  save_as_docx(path = "tbls/table2.docx")
 
-t1flex(tab2) %>% 
+t1flex(tab2) %>%
   bg(bg = "white", part = "all") %>%
-  save_as_image(path="tbls/table2.png")
+  save_as_image(path = "tbls/table2.png")
